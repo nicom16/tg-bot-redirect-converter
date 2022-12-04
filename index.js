cd999df43ec8491f57c06ee2fcf8dc5a577688da
Nicolò Mondini <nico.md.1600@gmail.com>
2022-12-04 13:32:49 +0100

    Added if for send=1

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ const PORT = process.env.PORT || 5000
 app.get('/', (req, res) => {
     const axios = require('axios');
     
-    axios.get(process.env.MANAGER, {
-        params: {
-            bot: req.query.bot,
-            send: req.query.send,
-        },
-    }).then((response) => {
-        console.log(response.data);
-    });
+    if (req.query.send !== undefined) {
+        axios.get(process.env.MANAGER, {
+            params: {
+                bot: req.query.bot,
+                send: req.query.send,
+            },
+        }).then((response) => {
+            console.log(response.data);
+        });
+    } else {
+        axios.get(process.env.MANAGER, {
+            params: {
+                bot: req.query.bot,
+            },
+        }).then((response) => {
+            console.log(response.data);
+        });
+    }
     
     res.send('Redirected!'); 
 }); 
