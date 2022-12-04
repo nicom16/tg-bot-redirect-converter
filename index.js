d9d090919a5a78ee4b7a2b6c22c8808f709a8be3
Nicolò Mondini <nico.md.1600@gmail.com>
2022-12-04 18:33:27 +0100

    Added "start script", improved error handling

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 // Express 
 const express = require('express'); 
 const app = express(); 
-const PORT = process.env.PORT || 5000 
+const PORT = process.env.PORT || 5000;
   
 app.get('/', (req, res) => {
     const axios = require('axios');
     
-//    if (req.query.send !== undefined) {
-        axios.get(process.env.MANAGER, {
-            params: {
-                bot: req.query.bot,
-                send: req.query.send,
-            },
-        }).then((response) => {
-            console.log(response.data);
-        });
- /*   } else {
-        axios.get(process.env.MANAGER, {
-            params: {
-                bot: req.query.bot,
-            },
-        }).then((response) => {
-            console.log(response.data);
-        });
-    }*/
-    
-    res.send(process.env.MANAGER); 
+    axios.get(process.env.MANAGER, {
+        params: {
+            bot: req.query.bot,
+            send: req.query.send,
+        },
+    }).then((response) => {
+        res.send(`Result: ${response.data}`);
+    }).catch((err) => {
+        res.send(err);
+    });
 }); 
   
 app.listen(PORT, () => { 
