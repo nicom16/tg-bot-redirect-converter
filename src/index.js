0a8f66c655708a7f7d794487203b63fc60531b9b
Nicolò Mondini <nico.md.1600@gmail.com>
2022-12-21 10:43:33 +0100

    Added /pep, created src/, testing functions.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 // Express 
 const express = require('express'); 
 const app = express(); 
+
+const axios = require('axios');    
+
+const ffmpeg = require('ffmpeg');
+
+const fs = require('fs');
+
 const PORT = process.env.PORT || 5000;
   
 app.get('/', (req, res) => {
-    const axios = require('axios');
-    
     axios.get(process.env.MANAGER, {
         params: {
             bot: req.query.bot,
@@ -17,7 +22,25 @@ app.get('/', (req, res) => {
         res.send(err);
     });
 }); 
-  
+
+app.get('/converter', (req, res) => {
+    res.send("Ok");
+});
+
+app.get('/pep', (req, res) => {
+    const axios = require('axios');
+    
+    axios.get(req.query.bot_page, {
+        params: {
+            send: req.query.send,
+        },
+    }).then((response) => {
+        res.send(`Result: ${response.data}`);
+    }).catch((err) => {
+        res.send(err);
+    });
+}); 
+
 app.listen(PORT, () => { 
   console.log(`App listening on port ${PORT}`); 
 });
