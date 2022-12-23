ddbc7f6a689ade24d820a0b0075fa85b1d996de4
Nicolò Mondini <nico.md.1600@gmail.com>
2022-12-23 12:47:53 +0100

    Added "/online" to check status

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
+app.get('/online', (req, res) => res.send("Online!"));
+
 app.get('/', (req, res) => {
     axios.get(process.env.MANAGER, {
         params: {
