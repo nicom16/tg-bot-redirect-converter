ed15e920a0dec30ccdfca987887f8d81dd6a7037
Nicolò <nico.md.1600@gmail.com>
2023-01-20 00:43:59 +0100

    Improved /requests, fixed indenting

--- src/functions.js
+++ src/functions.js
@@ -1,18 +1,18 @@
 // Functions
 
 async function downloadVideo(url, name) {
-    const axios = require('axios');
-    const fs = require('fs');
-    
-    const response = await axios.get(url, { responseType: "stream" });
-    const writer = fs.createWriteStream("./public/" + name);
-    await new Promise ((resolve, reject) => {
-        response.data.pipe(writer);
-        writer.on('finish', resolve);
-        writer.on('error', reject);
-    });
+  const axios = require('axios');
+  const fs = require('fs');
+  
+  const response = await axios.get(url, { responseType: "stream" });
+  const writer = fs.createWriteStream("./public/" + name);
+  await new Promise ((resolve, reject) => {
+    response.data.pipe(writer);
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
 }
 
 module.exports = {
-    downloadVideo
+  downloadVideo
 }
--- src/index.js
+++ src/index.js
@@ -17,85 +17,85 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
-app.get('/online', (req, res) => res.send("Online!"));
+app.get('/', (req, res) => res.send("Online!"));
 
-app.get('/', (req, res) => {
-    axios.get(process.env.MANAGER, {
-        params: {
-            bot: req.query.bot,
-            send: req.query.send,
-        },
-    }).then((response) => {
-        res.send(`Result: ${response.data}`);
-    }).catch((err) => {
-        res.send(err);
-    });
+app.get('/redirect', (req, res) => {
+  axios
+    .get(process.env.MANAGER, {
+      params: {
+        bot: req.query.bot,
+        send: req.query.send
+      }
+    })
+    .then((response) => res.send(`Result: ${response.data}`))
+    .catch((err) => res.send(err));
 }); 
 
 app.get('/converter', async (req, res) => {
-    const fs = require('fs');
+  const fs = require('fs');
 
-    var channel = process.env.CHANNEL;
-    var spec = req.query.spec;
+  var channel = process.env.CHANNEL;
+  var spec = req.query.spec;
 
-    var video_name = spec + ".mp4";
-    
-    var link = await bot.telegram.getFileLink(spec);
-    
-    functions.downloadVideo(link, video_name)
-        .then(() => new ffmpeg("./public/" + video_name))
-        .then((video) => video.setDisableAudio())
-        .then((gif) => gif.save("./public/no-" + video_name))
-        .then(() => bot.telegram.sendAnimation(channel, process.env.RAILWAY_STATIC_URL + "/no-" + video_name, { caption: process.env.CAPTION, parse_mode: "html" }))
-	.then((result) => {
-            fs.unlink("./public/" + video_name, (err) => err);
-            fs.unlink("./public/no-" + video_name, (err) => err);
-	    axios.get(process.env.ALTERVISTA, { 
-		params: {
-		    gifID_unique: req.query.unique,
-		    chatID: req.query.chat_id
-		}
-	    })
-	})
-        .catch((err) => console.log("Errore: " + err));
-    
-    res.send("Ok");
+  var video_name = spec + ".mp4";
+  
+  var link = await bot.telegram.getFileLink(spec);
+  
+  functions.downloadVideo(link, video_name)
+    .then(() => new ffmpeg("./public/" + video_name))
+    .then((video) => video.setDisableAudio())
+    .then((gif) => gif.save("./public/no-" + video_name))
+    .then(() => bot.telegram.sendAnimation(channel, process.env.RAILWAY_STATIC_URL + "/no-" + video_name, { caption: process.env.CAPTION, parse_mode: "html" }))
+    .then((result) => {
+      fs.unlink("./public/" + video_name, (err) => err);
+      fs.unlink("./public/no-" + video_name, (err) => err);
+      axios.get(process.env.ALTERVISTA, { 
+        params: {
+          gifID_unique: req.query.unique,
+          chatID: req.query.chat_id
+        }
+      })
+    })
+    .catch((err) => console.log("Errore: " + err));
+  
+  res.send("Ok");
 });
 
 app.get('/counter', async (req, res) => {
-    var bots = JSON.parse(process.env.BOTS);
-    
-    bots.forEach((bot) => {
-        axios
-          .get(bot, { params: { counter: true } })
-          .catch((err) => console.log(err));
-    });
+  var bots = JSON.parse(process.env.BOTS);
+  
+  bots.forEach((bot) => {
+    axios
+      .get(bot, { params: { counter: true } })
+      .catch((err) => console.log(err));
+  });
 
-    res.send("Counted!");
+  res.send("Counted!");
 });
 
 app.get('/requests', (req, res) => {
-    const axios = require('axios');
-    
-    axios
-      .get(req.query.url)
-      .catch((err) => console.log(err));
+  if (req.query.start) {
+    var bots = JSON.parse(process.env.BOTS_START);
+    bots.forEach((bot) => axios.get(bot))
 
-    res.send("Redirected!");
+    res.send("Started automatic requests acceptation and accepted pending invites!");
+  } else {
+    var bots = JSON.parse(process.env.BOTS_STOP);
+    bots.forEach((bot) => axios.get(bot))
+
+    res.send("Stopped automatic requests acceptation!");
+  }
 });
 
 app.get('/pep', (req, res) => {
-    const axios = require('axios');
-    
-    axios.get(req.query.bot_page, {
-        params: {
-            send: req.query.send,
-        },
-    }).then((response) => {
-        res.send(`Result: ${response.data}`);
-    }).catch((err) => {
-        res.send(err);
-    });
+  axios
+    .get(req.query.bot_page, {
+      params: {
+        send: req.query.send
+      }
+    })
+    .then((response) => res.send(`Result: ${response.data}`))
+    .catch((err) => res.send(err));
 }); 
 
 app.listen(PORT, () => { 
