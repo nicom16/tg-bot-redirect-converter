66b9552ba9c26d5824cdeab85680627a14c64604
Nicolò <nico.md.1600@gmail.com>
2023-03-09 15:11:18 +0100

    Fixed axios.all

--- src/index.js
+++ src/index.js
@@ -100,14 +100,18 @@ app.get('/pep', (req, res) => {
 
 app.get('/pep-requests-on', (req, res) => {
   var pep = JSON.parse(process.env.PEPON); 
-  axios.all(pep.map((url) => axios.get(url)));
+  axios.all(pep.map((url) => axios.get(url)).then(
+    (data) => console.log(data)
+  ));
 
   res.send("Ok!");
 });
 
 app.get('/pep-requests-off', (req, res) => {
   var pep = JSON.parse(process.env.PEPOFF); 
-  axios.all(pep.map((url) => axios.get(url)));
+  axios.all(pep.map((url) => axios.get(url)).then(
+    (data) => console.log(data)
+  ));
 
   res.send("Ok!");
 });
