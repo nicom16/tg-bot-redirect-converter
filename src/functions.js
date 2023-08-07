5eab5f90aca0aab8a61c06afc2e1679b3828f7a6
Nicolò <nico.md.1600@gmail.com>
2023-08-08 00:41:51 +0200

    Fixed routes, removed useless packages

--- src/functions.js
+++ src/functions.js
@@ -1,15 +1,19 @@
 // Functions
 
-async function downloadVideo(url, name) {
+async function downloadVideo(url, path) {
   const axios = require('axios');
   const fs = require('fs');
-  
-  const response = await axios.get(url, { responseType: "stream" });
-  const writer = fs.createWriteStream("./public/" + name);
-  await new Promise ((resolve, reject) => {
-    response.data.pipe(writer);
-    writer.on('finish', resolve);
-    writer.on('error', reject);
+
+  const video = await axios.get(url, { responseType: "stream" });
+  const stream = video.data.pipe(fs.createWriteStream(path));
+
+  return await new Promise ((resolve, reject) => {
+    stream
+      .on('finish', () => {
+        console.log("Video downloaded!");
+        resolve();
+      })
+      .on('error', (err) => reject(err));
   });
 }
 
--- src/index.js
+++ src/index.js
@@ -1,69 +1,93 @@
 // Index
-require('dotenv').config()
-
 const express = require('express'); 
 const app = express(); 
 
-const axios = require('axios');    
-
-const ffmpeg = require('ffmpeg');
-
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.TOKEN);
+const bot = new Telegraf(process.env.UMORE_TOKEN);
 
-const functions = require('./functions.js');
-
-const PORT = process.env.PORT || 5000;
+const PORT = 8080;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send("Online!"));
 
 app.get('/redirect', (req, res) => {
+  
+  const axios = require('axios');    
+  
   axios
     .get(process.env.MANAGER, {
       params: {
         bot: req.query.bot,
-        send: req.query.send
+        send: req.query.send,
+        type: req.query.type
       }
     })
     .then((response) => res.send(`Result: ${response.data}`))
     .catch((err) => res.send(err));
+
 }); 
 
+app.get('/clean', (req, res) => {
 
-// Main routes
-app.get('/converter', async (req, res) => {
+  const fs = require("fs");
+  const path = require("path");
+
+  const directory = "public";
+
+  fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join(directory, file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+
+  res.send("Cleaned public/ directory!");
+
+});
+
+app.get('/converter', (req, res) => {
+
+  const functions = require('./functions.js');
   const fs = require('fs');
+  const ffmpeg = require('./ffmpeg/index.js');
+  const axios = require('axios');    
 
-  var channel = process.env.CHANNEL;
-  var spec = req.query.spec;
+  const channel = process.env.UMORE_CHANNEL;
+  const spec = req.query.spec;
 
-  var video_name = spec + ".mp4";
+  const videoName = spec + ".mp4";
+  const videoPath = "./public/" + videoName;
+  const gifPath = "./public/no-" + videoName;
   
-  var link = await bot.telegram.getFileLink(spec);
-  
-  functions.downloadVideo(link, video_name)
-    .then(() => new ffmpeg("./public/" + video_name))
-    .then((video) => video.setDisableAudio())
-    .then((gif) => gif.save("./public/no-" + video_name))
-    .then(() => bot.telegram.sendAnimation(channel, process.env.RAILWAY_STATIC_URL + "/no-" + video_name, { caption: process.env.CAPTION, parse_mode: "html" }))
-    .then((result) => {
-      fs.unlink("./public/" + video_name, (err) => err);
-      fs.unlink("./public/no-" + video_name, (err) => err);
-      axios.get(process.env.ALTERVISTA, { 
+  bot.telegram.getFileLink(spec)
+    .then((url) => functions.downloadVideo(url, videoPath))
+    .then(() => new ffmpeg(videoPath))
+    .then((video) => video.setDisableAudio().setVideoCodec('copy').save(gifPath))
+    .then(() => bot.telegram.sendAnimation(channel, process.env.STATIC_URL + "/no-" + videoName, { caption: process.env.UMORE_CAPTION, parse_mode: "html" }))
+    .then(() => {
+      fs.unlink(videoPath, (err) => err);
+      fs.unlink(gifPath, (err) => err);
+      axios.get(process.env.UMORE_ALTERVISTA, { 
         params: {
           gifID_unique: req.query.unique,
           chatID: req.query.chat_id
         }
       })
     })
-    .catch((err) => console.log("Errore: " + err));
-  
+    .catch((err) => console.log(err));
+
   res.send("Ok");
+
 });
 
 app.get('/counter', async (req, res) => {
+  
+  const axios = require('axios');    
+
   var bots = JSON.parse(process.env.BOTS);
   
   bots.forEach((bot) => {
@@ -73,9 +97,13 @@ app.get('/counter', async (req, res) => {
   });
 
   res.send("Counted!");
+
 });
 
 app.get('/requests', (req, res) => {
+
+  const axios = require('axios');    
+
   if (req.query.start) {
     var bots = JSON.parse(process.env.BOTS_START);
     bots.forEach((bot) => axios.get(bot))
@@ -87,11 +115,15 @@ app.get('/requests', (req, res) => {
 
     res.send("Stopped automatic requests acceptation!");
   }
+
 });
 
 
 // Routes for pep-app
 app.get('/pep', (req, res) => {
+
+  const axios = require('axios');    
+
   axios
     .get(req.query.bot_page, {
       params: {
@@ -100,25 +132,32 @@ app.get('/pep', (req, res) => {
     })
     .then((response) => res.send(`Result: ${response.data}`))
     .catch((err) => res.send(err));
+
 }); 
 
 app.get('/pep-requests-on', (req, res) => {
+  
+  const axios = require('axios');    
+  
   var pep = JSON.parse(process.env.PEPON); 
   
   axios
     .get(pep[req.query.index])
     .then((response) => res.send(`Ok: ${response}`));
+
 });
 
 app.get('/pep-requests-off', (req, res) => {
+  
+  const axios = require('axios');    
+
   var pep = JSON.parse(process.env.PEPOFF); 
   
   axios
     .get(pep[req.query.index])
     .then((response) => res.send(`Ok: ${response}`));
+
 });
 
-app.listen(PORT, () => { 
-  console.log(`App listening on port ${PORT}`); 
-});
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
